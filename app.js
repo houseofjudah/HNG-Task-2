dd2c8f0d0f1e6a8b257901161977d2de356202fe
houseofjudah <judah.onunkwor.202828@unn.edu.ng>
2024-07-08 22:00:41 +0100

    third commit

--- app.js
+++ app.js
@@ -2,12 +2,27 @@ const express = require('express');
 const app = express();
 const authRoute = require('./routes/auth.route');
 const organisationRoute = require('./routes/org.route');
-const {sequelize, models} = require('./database').sequelize;
+const {sequelize, models} = require('./database');
 
 app.use(express.json());
 
 app.use('/auth', authRoute);
 app.use('/api', organisationRoute);
+
+
+
+
+const { Organisation, User } = models;
+
+
+Organisation.findAll().then(organisations => {
+  console.log(organisations);
+});
+
+User.findAll().then(user => {
+  console.log(user);
+});
+
 
 const port = process.env.PORT
 app.listen(port, () => {
